cc96c7b462691914696705daea3bb3ff2a6d0cde
caelumean215 <caelumean215@gmail.com>
2023-05-17 18:02:39 +0900

    프로젝트 설정완료

--- CalEngine/Engine_SOURCE/Engine_SOURCE.cpp
+++ CalEngine/Engine_SOURCE/Engine_SOURCE.cpp
@@ -1,180 +1,56 @@
-﻿// Engine_SOURCE.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+﻿// Engine_SOURCE.cpp : 정적 라이브러리를 위한 함수를 정의합니다.
 //
 
 #include "framework.h"
-#include "Engine_SOURCE.h"
 
-#define MAX_LOADSTRING 100
-
-// 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
-
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+// TODO: 라이브러리 함수의 예제입니다.
+void fnEngineSOURCE()
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: 여기에 코드를 입력합니다.
-
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_ENGINESOURCE, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_ENGINESOURCE));
-
-    MSG msg;
-
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
 }
 
+// 라이브러리란?
+// 자주 사용되는 기능이나 함수 서브루틴등등등 가르켜서 총제적으로
+// 구현해놓은 프로젝트이다.
+
+// 동적 라이브러리
+// xxxx.dll
+// 프로그램 시작하고 있는동안에 해당 라이브러리를 참조하여
+// 실행한다.
+// 하나의 메모리영역에 올려두고 여러프로그램들이 나누어 사용하는
+// 라이브러리
+// 
+
+// 장점
+// 메모리를 절약할수있다.
+// 프로그램에 한번 올려진 메모리를 공유하므로 사용하므로 사용공간자체가 적다.
+// 업그레이드(패치)를 비교적 정적라이브러리보다 쉽게 진행 가능하다.
+// 실행 중간에 패치가 가능하다.
+
+// 단점
+// 외부의존도가 높아진다.
+// 공유 라리브러리를 메모리 올리려면 찾고 검색하는데 시간이 걸리므로
+// 성능저하가 생긴다.
 
 
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
+// 정적 라이브러리
+// 프로그램을 컴파일하는 과정에 내가 작성한 라이브러리코드들이
+// 포함이된다.
+// xxxxx.lib
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+// 라이브러리 코드가 바뀌면 다시 빌드 또는
+// 다시 배포를 해줘야한다. 패치해줘야한다.
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ENGINESOURCE));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_ENGINESOURCE);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
-}
+// lib 는 링크(linking)시 필요하고 dll 프로그램 실행시 필요하다
 
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+// 장점 
+// 한번완성이되면 안정적으로 프로그램 사용이 가능하다.
+// 컴파일시  필요한 라이브러리를 프로그램내에 적재하기 떄문에
+// 앱을 안정적으로 활용할수 있게된다.
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
-}
+// 런타임시 외부를 참조할 필요가 없기떄문에 속도에서 장점이 있다.
+// 
+// 단점
+// 라이브러리 변깅이 필요할시 변경된 라이브러리 재배포하면안되고
+// 모든 프로그램 처음부터 다시 빌드하여
+// 새로운버전으로 배포를 진행해줘야한다.
